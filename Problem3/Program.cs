06c68c862fb07893017b09b66d9a1a6afbb282b2
HGC <harry.cummings@softwire.com>
2013-03-14 20:55:03 +0000

    End of Problem 3

--- Problem3/Program.cs
+++ Problem3/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +11,43 @@ namespace Problem3
 {
   class Program
   {
+    private const int Stride = 3;
+
+    private static Color[,] state;
+    private static int width;
+    private static int height;
+
     static void Main(string[] args)
     {
+      var bitmap = (Bitmap)Image.FromFile("ImageWithHiddenContent.bmp");
+
+      var hiddenBitmap = new Bitmap(width = bitmap.Width / Stride, height = bitmap.Height / Stride);
+
+      for (int i = 0; i < width; i++)
+      {
+        for (int j = 0; j < height; j++)
+        {
+          var red = 0;
+          var green = 0;
+          var blue = 0;
+
+          int mask = 1;
+
+          for (var bit = 0; bit < 8; bit++)
+          {
+            var currentPixel = bitmap.GetPixel((i*3) + (bit%3), (j*3) + (bit/3));
+
+            red += (currentPixel.R & mask) << bit;
+            green += (currentPixel.G & mask) << bit;
+            blue += (currentPixel.B & mask) << bit;
+          }
+
+          hiddenBitmap.SetPixel(i, j, Color.FromArgb(255, red, green, blue));
+        }
+      }
+
+
+      hiddenBitmap.Save("hiddenimage.bmp");
     }
   }
 }
