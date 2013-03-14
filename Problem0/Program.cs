5b9eda55d0c7a06581101fb214174b89664c4145
HGC <harry.cummings@softwire.com>
2013-03-14 18:41:39 +0000

    End of problem 1

---
+++ Problem0/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace Problem0
+{
+  public class Program
+  {
+    static void Main()
+    {
+      var number = Convert.ToInt32("0xFF44", 16);
+      Console.WriteLine((number >> 8).ToString("X"));
+
+
+      /*// Read a load of lines from a file
+      foreach (string inputLine in File.ReadAllLines(@"FileList.csv"))
+      {
+        // Do some naive CSV parsing
+        string[] sourceAndDestination = inputLine.Split(',');
+        string sourceFilename = sourceAndDestination[0];
+        string destinationFilename = sourceAndDestination[1];
+
+        // Read a bitmap from disk
+        // Note that this code requires you to add a reference to System.Drawing to your project
+        var bitmap = (Bitmap)Image.FromFile(sourceFilename);
+
+        // Do some pixel-level manipulation of the bitmap, fiddling with the colours using RGB values
+        int centreX = bitmap.Width/2;
+        int centreY = bitmap.Height/2;
+
+        var oldColour = bitmap.GetPixel(centreX, centreY);
+
+        var newColour = Color.FromArgb(oldColour.R/2, oldColour.G, oldColour.B*2);
+
+        bitmap.SetPixel(centreX, centreY, newColour);
+
+        // Save the resultant bitmap to disk
+        bitmap.Save(destinationFilename);
+
+        // Display some sort of output to some sort of console so you have something to send to MWR as your "answer"
+        Console.WriteLine("The pixel was this red: " + newColour.R);
+    }
+       */
+    }
+  }
+}
---
+++ Problem1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var answer = Enumerable
+        .Range(0, 1000000)
+        .Aggregate(0, (cumulative, current) => IsKaprekarNumber(current) ? cumulative + 1 : cumulative);
+
+      Console.WriteLine("ANSWER:");
+      Console.WriteLine(answer);
+      Console.ReadLine();
+    }
+
+    private static bool IsKaprekarNumber(long current)
+    {
+      long square = current * current;
+
+      int digits = square.ToString("X").Length;
+
+      for (int splitPoint = 0; splitPoint < digits; splitPoint++)
+      {
+        int shift = 4*splitPoint;
+        var upperPart = square >> (shift);
+        var lowerPart = square - (upperPart << (shift));
+        if (lowerPart > 0 & upperPart > 0 && lowerPart + upperPart == current)
+        {
+          Console.WriteLine("{0} squared = {1}, {0} = {2} + {3}", current.ToString("X"), square.ToString("X"), upperPart.ToString("X"), lowerPart.ToString("X"));
+          return true;
+        }
+      }
+
+      return false;
+    }
+  }
+}
